f8d5f40f8c61cd3b6bc4133744fa17ca36773e91
Alfredkoome <koome.26551@students.kyu.ac.ke>
2025-11-10 13:11:28 +0300

    Implement gross pay and tax calculations
    Fixed calculation of gross pay and added tax logic.

--- CAT2.Q2.c
+++ CAT2.Q2.c
@@ -6,9 +6,25 @@
 #include<stdio.h>
 int main() {
     float hours,rate,grosspay,taxes,netpay;
-
     printf("Enter the hours worked in a week : ");
     scanf("%f",&hours);
-    printf("Enter hourly wag : ");
+    printf("Enter hourly wage : ");
+ scanf("%f",&rate);
+
+ if (hours>40){
+  grospay=40*rate+(hours-40)*rate*1.5;
+ }else {
+  grosspay=hours*rate
+ }
+ if (grosspay<=600){
+  taxes=0.15*grosspay;
+ }else{
+  taxes=0.15*600+0.20*(grosspay-600);
+ }
+ netpay=grosspay-taxes;
+ printf("grosspay:$%.2f\n",grosspay);
+ printf("taxes:$%.2f\n",taxes);
+ printf("net pay:$%.2f\n",netpay);
     return 0;
 }
+
